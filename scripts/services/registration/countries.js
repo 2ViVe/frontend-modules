a02ea8c8ced93823c95eee01de9680b35b219273
Liu Yang <zation1@gmail.com>
2014-05-24 13:22:30 +0800

    make countries as a singleton

--- scripts/services/registration/countries.js
+++ scripts/services/registration/countries.js
@@ -1,31 +1,36 @@
 'use strict';
 
 angular.module('2ViVe')
-  .factory('Registration.Countries', ['$http', 'DEFAULT_COUNTRY_ID',
-    function($http, DEFAULT_COUNTRY_ID) {
+  .factory('Registration.Countries', ['$http', 'DEFAULT_COUNTRY_ID', '$q',
+    function($http, DEFAULT_COUNTRY_ID, $q) {
+      var Countries = {
+        data: [],
+        fetch: function() {
+          var deferred = $q.defer(),
+            promise = deferred.promise;
 
-      function Countries() {
-        this.data = [];
-      }
-
-      Countries.prototype.fetch = function() {
-        var countries = this;
-        return $http.get('/api/v2/registrations/countries', { cache: true })
-          .then(function(response) {
-            countries.data = response.data.response;
-            return countries;
-          });
-      };
-
-      Countries.prototype.defaultCountry = function() {
-        var result = null;
-        angular.forEach(this.data, function(country) {
-          if (country.id === DEFAULT_COUNTRY_ID) {
-            result = country;
-            return null;
+          if (Countries.data.length > 0) {
+            deferred.resolve(Countries);
+          } else {
+            $http.get('/api/v2/registrations/countries', { cache: true })
+              .then(function(response) {
+                Countries.data = response.data.response;
+                deferred.resolve(Countries);
+              });
           }
-        });
-        return result;
+
+          return promise;
+        },
+        defaultCountry: function() {
+          var result = null;
+          angular.forEach(Countries.data, function(country) {
+            if (country.id === DEFAULT_COUNTRY_ID) {
+              result = country;
+              return null;
+            }
+          });
+          return result;
+        }
       };
 
       return Countries;
