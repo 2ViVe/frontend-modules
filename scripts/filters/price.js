1643dfe6ce9546e9c3bbe3f96f9940c5795fc17e
Liu Yang <zation1@gmail.com>
2014-06-08 14:14:00 +0800

    handle price when not a number

--- scripts/filters/price.js
+++ scripts/filters/price.js
@@ -3,6 +3,9 @@
 angular.module('2ViVe')
   .filter('price', ['CURRENCY_SYMBOL', function(CURRENCY_SYMBOL) {
     return function(price) {
+      if (isNaN(price)) {
+        return '--';
+      }
       return CURRENCY_SYMBOL + parseFloat(Math.round(price * 100) / 100).toFixed(2);
     };
   }]);
