9a9ad35b1edd2525e0cb87a64b9266842c339333
Liu Yang <zation1@gmail.com>
2014-06-11 21:19:37 +0800

    add next and prev method

--- scripts/directives/slider.js
+++ scripts/directives/slider.js
@@ -9,17 +9,26 @@ angular.module('2ViVe')
           var isAutoPlay = attrs.isAutoPlay === 'true' || attrs.isAutoPlay === undefined;
           var delay = attrs.delay === undefined && isAutoPlay ? 7000 : attrs.delay;
           var hasDots = attrs.hasDots === 'true' || attrs.hasDots === undefined;
+          var slider;
+
+          scope.nextSlide = function() {
+            slider.next();
+          };
+
+          scope.previousSlide = function() {
+            slider.prev();
+          };
 
           $timeout(function() {
             var images = element.find('img');
             images.hide();
             images.on('load', function() {
               images.show();
-              angular.element(element).unslider({
+              slider = angular.element(element).unslider({
                 dots: hasDots,
                 autoplay: isAutoPlay,
                 delay: delay
-              });
+              }).data('unslider');
             });
           });
         }
