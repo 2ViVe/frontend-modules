3de35443cc37e7962ceb97dcebb2a9743d1b80d5
Liu Yang <zation1@gmail.com>
2014-06-11 19:37:14 +0800

    fix jshint

--- scripts/directives/requireWithValidator.js
+++ scripts/directives/requireWithValidator.js
@@ -1,26 +1,27 @@
+'use strict';
+
 angular.module('2ViVe')
-  .directive('requireWith', ['$http',
-    function ($http) {
-      return {
-        restrict: 'A',
-        require: 'ngModel',
-        scope: {
-          requireWith: '='
-        },
-        link: function (scope, element, attrs, ctrl) {
-          ctrl.$parsers.push(atLeastOneFieldNotEmpty);
-          ctrl.$formatters.push(atLeastOneFieldNotEmpty);
-
-          scope.$watch('requireWith', function() {
-            atLeastOneFieldNotEmpty(ctrl.$modelValue);
-          });
-
-          function atLeastOneFieldNotEmpty(value) {
-            var isOneOfThemFilled = !ctrl.$isEmpty(scope.requireWith) || !ctrl.$isEmpty(value);
-            ctrl.$setValidity('requireWith', isOneOfThemFilled);
-            return value;
-          }
+  .directive('requireWith', function() {
+    return {
+      restrict: 'A',
+      require: 'ngModel',
+      scope: {
+        requireWith: '='
+      },
+      link: function(scope, element, attrs, ctrl) {
+        function atLeastOneFieldNotEmpty(value) {
+          var isOneOfThemFilled = !ctrl.$isEmpty(scope.requireWith) || !ctrl.$isEmpty(value);
+          ctrl.$setValidity('requireWith', isOneOfThemFilled);
+          return value;
         }
-      };
-    }]);
+
+        ctrl.$parsers.push(atLeastOneFieldNotEmpty);
+        ctrl.$formatters.push(atLeastOneFieldNotEmpty);
+
+        scope.$watch('requireWith', function() {
+          atLeastOneFieldNotEmpty(ctrl.$modelValue);
+        });
+      }
+    };
+  });
 
