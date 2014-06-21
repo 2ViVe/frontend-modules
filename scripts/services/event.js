b9fdb79ac14a2237d4ba1e89cc8bb934453d7bfe
Liu Yang <zation1@gmail.com>
2014-06-21 13:08:23 +0800

    add fetch event by user id

--- scripts/services/event.js
+++ scripts/services/event.js
@@ -177,6 +177,17 @@ angular.module('2ViVe')
           });
         },
 
+        fetchByUserId: function(userId) {
+          return $http.get('/api/v2/events', {
+            transformResponse: camelCaselize,
+            params: {
+              'user-id': userId
+            }
+          }).then(function(response) {
+            return response.data.response;
+          });
+        },
+
         fetchAll: function() {
           return $http.get('/api/v2/events', {
             transformResponse: camelCaselize
