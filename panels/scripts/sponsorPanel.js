3b22b5801ac8aed4a760a2428d8085a4e414233c
xlaok <gaoyixin@joymap.cn>
2014-06-23 23:58:55 +0800

    handler-locate

--- panels/scripts/sponsorPanel.js
+++ panels/scripts/sponsorPanel.js
@@ -10,8 +10,40 @@ angular
         submitted: '=',
         required: '@'
       },
-      controller: ['$scope', function($scope) {
+      controller: ['$scope', '$modal', function($scope, $modal) {
         $scope.$errorMessages = {};
+        $scope.searchHandler = function(){
+          $modal.open({
+            templateUrl: 'bower_components/2ViVe/panels/views/handler-locate.html',
+            controller: 'HandlerLocateModalController'
+          }).result.then(function(result){
+              $scope.account.sponsor = result.distributorId;
+              console.log(result);
+            });
+        };
       }]
     };
+  }])
+  .controller('HandlerLocateModalController', ['$scope', '$location', 'Handlers', '$modalInstance', function($scope, $location, Handlers, $modalInstance) {
+    $scope.input = {};
+    $scope.submit = function(){
+      Handlers.fetch($scope.input.microchipId, $scope.input.firstName, $scope.input.lastName).then(function(results){
+        $scope.results = results;
+      });
+
+    };
+    $scope.changeHandler = function(handler){
+      $scope.targetHandler = handler;
+    };
+
+    $scope.connect = function(){
+      if ($scope.targetHandler !== '') {
+        $modalInstance.close($scope.targetHandler);
+      }
+    };
+    $scope.cancel = function() {
+      $modalInstance.dismiss('cancel');
+    };
+    $scope.results = [];
+    $scope.targetHandler = '';
   }]);
