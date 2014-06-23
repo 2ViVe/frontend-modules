aadd98b464c49d36bab1a2a45226c2bf9844b33c
Liu Yang <zation1@gmail.com>
2014-06-23 19:32:30 +0800

    add option for price filter

--- scripts/filters/price.js
+++ scripts/filters/price.js
@@ -2,9 +2,12 @@
 
 angular.module('2ViVe')
   .filter('price', ['CURRENCY_SYMBOL', function(CURRENCY_SYMBOL) {
-    return function(price) {
+    return function(price, options) {
       if (isNaN(price)) {
         return '--';
+      }
+      if (options === 'with out currency') {
+        CURRENCY_SYMBOL = '';
       }
       return CURRENCY_SYMBOL + parseFloat(Math.round(price * 100) / 100).toFixed(2);
     };
