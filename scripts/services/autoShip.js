30282886ebe16ae8feffeacd43e2335ed7a81681
Liu Yang <zation1@gmail.com>
2014-07-23 13:53:05 +0800

    add orderSummary for autoShip

--- scripts/services/autoShip.js
+++ scripts/services/autoShip.js
@@ -1,11 +1,38 @@
 'use strict';
 
 angular.module('2ViVe')
+  .factory('AutoShip', ['$http', 'Dashlize', 'CamelCaseLize',
+    function($http, dashlize, camelCaselize) {
+      var AutoShip = function() {};
+
+      AutoShip.prototype = {
+        orderSummary: function(autoShipItems, shippingAddress, shippingMethodId) {
+          var autoShip = this;
+          return $http.post('/api/v2/autoships/orders/summary', {
+            'autoship-items': autoShipItems,
+            'shipping-address': shippingAddress,
+            'shipping-method-id': shippingMethodId
+          }, {
+            transformResponse: camelCaselize,
+            transformRequest: function(data) {
+              return angular.toJson(dashlize(data));
+            }
+          }).then(function(response) {
+            autoShip.orderSummary = response.data.response;
+            return autoShip;
+          });
+        }
+      };
+
+      return AutoShip;
+    }
+  ])
   .factory('AutoShips', ['$http', 'Dashlize', 'CamelCaseLize',
     function($http, dashlize, camelCaselize) {
       var AutoShips = function() {};
 
       AutoShips.prototype = {
+
         fetch: function() {
           var autoShips = this;
           return $http.get('/api/v2/autoships', {
