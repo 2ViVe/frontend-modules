013891d0c3231c869a55b4927bebd530beaddc55
Liu Yang <zation1@gmail.com>
2014-07-23 13:47:41 +0800

    add require and move year month data into controller

--- panels/scripts/creditCardPanel.js
+++ panels/scripts/creditCardPanel.js
@@ -9,7 +9,36 @@ angular.module('2ViVe')
       },
       replace: true,
       scope: {
+        submitted: '=',
         creditcard: '='
-      }
+      },
+      controller: ['$scope', function($scope) {
+        $scope.expirationYears = [];
+        $scope.expirationMonths = [];
+        var currentYear = moment().year();
+        var maxYear = currentYear + 20;
+        for (var year = currentYear; year < maxYear; year++) {
+          $scope.expirationYears.push(year);
+        }
+        for (var month = 1; month < 13; month++) {
+          $scope.expirationMonths.push(month);
+        }
+
+        $scope.onYearChange = function(year) {
+          var month = 1;
+
+          while($scope.expirationMonths.length) {
+            $scope.expirationMonths.pop();
+          }
+
+          if (year === currentYear) {
+            month = moment().month() + 1;
+          }
+
+          for (; month < 13; month++) {
+            $scope.expirationMonths.push(month);
+          }
+        };
+      }]
     };
   }]);
