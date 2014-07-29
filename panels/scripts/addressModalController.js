ca8eadb253b2ea0e02ff14242a277d395caa4737
Liu Yang <zation1@gmail.com>
2014-07-29 16:29:55 +0800

    add address modal controller

---
+++ panels/scripts/addressModalController.js
@@ -0,0 +1,26 @@
+'use strict';
+
+angular
+  .module('2ViVe')
+  .controller('AddressModalController', ['$scope', '$modalInstance', 'Address', 'address', 'type', 'title',
+    function($scope, $modalInstance, Address, address, type, title) {
+      $scope.type = type;
+      $scope.title = title;
+      $scope.address = Address.create($scope.type);
+      $scope.address.fullFill(address);
+
+      $scope.submit = function() {
+        if ($scope.submitted || this.form.$valid) {
+
+          $scope.address.validate()
+            .then(function() {
+              $modalInstance.close($scope.address);
+            });
+        }
+        $scope.submitted = true;
+      };
+
+      $scope.cancel = function() {
+        $modalInstance.dismiss('cancel');
+      };
+    }]);
