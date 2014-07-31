755c99ccc3630405439913b031dad139165a00b9
Liu Yang <zation1@gmail.com>
2014-07-31 16:11:52 +0800

    move pagination status into directive

--- pagination/controller.js
+++ pagination/controller.js
@@ -9,7 +9,7 @@ angular.module('2ViVe')
         return templateUrl ? templateUrl : 'bower_components/2ViVe/pagination/view.html';
       },
       scope: {
-        hidePageNumber: '@',
+        hidePageNumber: '=',
         numberPerPage: '=',
         startPage: '@',
         refresh: '=',
@@ -18,60 +18,69 @@ angular.module('2ViVe')
         onPreviousPage: '=',
         onGoToPage: '=',
         templateUrl: '@',
-        limitArr : '='
+        availableNumberPerPage: '@'
       },
-      controller: ['$scope', '$attrs', '$timeout', function($scope, $attrs, $timeout) {
+      controller: ['$scope', '$attrs', function($scope, $attrs) {
         function refresh(total) {
-          $scope.pageNumber = Math.ceil(total / $scope.numberPerPage);
+          refreshPage(total, $scope.limit);
           if ($scope.startPage === 'first') {
             $scope.currentPage = 1;
             $scope.offset = 0;
           } else if ($scope.startPage === 'last') {
             $scope.currentPage = $scope.pageNumber;
-            $timeout(function() {
-              $scope.offset = total - 1;
-              $scope.onGoToPage($scope.offset, $scope.numberPerPage);
-            });
+            $scope.offset = total - 1;
           }
+        }
 
+        function refreshPage(total, limit) {
+          $scope.pageNumber = Math.ceil(total / limit);
           $scope.pages = [];
           for (var i = 1; i <= $scope.pageNumber; i++) {
             $scope.pages.push(i);
           }
         }
 
-        if ($attrs.refresh !== undefined) {
-          $scope.refresh = refresh;
+        $scope.limit = parseInt($scope.numberPerPage);
+        $scope.limits = [];
+        if ($attrs.availableNumberPerPage !== undefined) {
+          angular.forEach($scope.availableNumberPerPage.split(','), function(limit) {
+            $scope.limits.push(parseInt(limit));
+          });
         }
 
         if ($scope.startPage === undefined) {
           $scope.startPage = 'first';
         }
-        refresh($scope.total);
+
+        $scope.refresh = refresh;
 
         $scope.goTo = function(page) {
           $scope.currentPage = page;
           $scope.offset = ($scope.currentPage - 1) * 25;
-          $scope.onGoToPage($scope.offset, $scope.numberPerPage);
+          $scope.onGoToPage(page, $scope.offset, $scope.limit);
         };
 
         $scope.previousPage = function() {
           if ($scope.currentPage > 1) {
             $scope.currentPage--;
-            $scope.onPreviousPage($scope.currentPage);
+            $scope.onPreviousPage($scope.currentPage, $scope.offset, $scope.limit);
           }
         };
 
         $scope.nextPage = function() {
           if ($scope.currentPage < $scope.pageNumber) {
             $scope.currentPage++;
-            $scope.onNextPage($scope.currentPage);
+            $scope.onNextPage($scope.currentPage, $scope.offset, $scope.limit);
           }
         };
 
-        $scope.updateLimit = function(){
-          $scope.onGoToPage($scope.offset, $scope.numberPerPage);
-        }
+        $scope.updateLimit = function() {
+          refreshPage($scope.total, $scope.limit);
+          if ($scope.currentPage > $scope.pageNumber) {
+            $scope.currentPage = $scope.pageNumber;
+          }
+          $scope.onGoToPage($scope.currentPage, $scope.offset, $scope.limit);
+        };
       }]
     };
   });
