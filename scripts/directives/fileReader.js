c7ff98f4b0c14af7e986315f3293256b17405496
Liu Yang <zation1@gmail.com>
2014-08-07 11:43:41 +0800

    fix file reader

--- scripts/directives/fileReader.js
+++ scripts/directives/fileReader.js
@@ -1,28 +1,31 @@
-.directive('viveFileReader', ['Avatar',
-  function(Avatar) {
-    return {
-      restrict: 'E',
-      scope: {
-        onUploaded: '='
-      },
-      link: function(scope, element) {
-        var $element = angular.element(element),
-          $fileInput = $element.find('input');
+'use strict';
 
-        $element.find('button').on('click', function() {
-          $fileInput.click();
-        });
+angular.module('2ViVe')
+  .directive('viveFileReader', ['Avatar',
+    function(Avatar) {
+      return {
+        restrict: 'E',
+        scope: {
+          onUploaded: '='
+        },
+        link: function(scope, element) {
+          var $element = angular.element(element),
+            $fileInput = $element.find('input');
 
-        $fileInput.on('change', function(changeEvent) {
-          var file = changeEvent.target.files;
-          if (file.length === 0) {
-            return;
-          }
-          Avatar.upload(file[0]).then(function(reslut) {
-            scope.onUploaded(reslut.data.response['image-url']);
+          $element.find('button').on('click', function() {
+            $fileInput.click();
           });
-        });
-      }
-    };
-  }
-]);
+
+          $fileInput.on('change', function(changeEvent) {
+            var file = changeEvent.target.files;
+            if (file.length === 0) {
+              return;
+            }
+            Avatar.upload(file[0]).then(function(reslut) {
+              scope.onUploaded(reslut.data.response['image-url']);
+            });
+          });
+        }
+      };
+    }
+  ]);
