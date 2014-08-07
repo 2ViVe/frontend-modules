90df21aed3b90c323675bc85e032e0b192459a06
Liu Yang <zation1@gmail.com>
2014-08-07 10:56:19 +0800

    add file reader

---
+++ scripts/directives/fileReader.js
@@ -0,0 +1,28 @@
+.directive('viveFileReader', ['Avatar',
+  function(Avatar) {
+    return {
+      restrict: 'E',
+      scope: {
+        onUploaded: '='
+      },
+      link: function(scope, element) {
+        var $element = angular.element(element),
+          $fileInput = $element.find('input');
+
+        $element.find('button').on('click', function() {
+          $fileInput.click();
+        });
+
+        $fileInput.on('change', function(changeEvent) {
+          var file = changeEvent.target.files;
+          if (file.length === 0) {
+            return;
+          }
+          Avatar.upload(file[0]).then(function(reslut) {
+            scope.onUploaded(reslut.data.response['image-url']);
+          });
+        });
+      }
+    };
+  }
+]);
