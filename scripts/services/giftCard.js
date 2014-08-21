d54adf94dad427356af53782c9ac6b146ecd71ba
Liu Yang <zation1@gmail.com>
2014-08-21 14:56:56 +0800

    add event binding for gift card

--- scripts/services/giftCard.js
+++ scripts/services/giftCard.js
@@ -14,7 +14,7 @@ angular.module('2ViVe')
         });
       };
 
-      GiftCards.resendEmail = function(code){
+      GiftCards.resendEmail = function(code) {
         var url = '/api/v2/giftcards/' + code + '/emails';
         return $http.post(url, {}, {
           transformResponse: camelcase
@@ -55,17 +55,18 @@ angular.module('2ViVe')
         return deferred.promise;
       };
 
-      GiftCard.prototype.purchase = function(selectedGiftCard, info) {
+      GiftCard.prototype.purchase = function(selectedGiftCard, info, path) {
         ipCookie('selectedGiftCard', selectedGiftCard, {
           domain: domain
         });
         ipCookie('giftCardInfo', info, {
           domain: domain
         });
+        path = path ? path : '/gift/checkout';
         if (User.isLogin) {
-          $location.path('/gift/checkout');
+          $location.path(path);
         } else {
-          LocalStorage.setPathAfterLogin('/gift/checkout');
+          LocalStorage.setPathAfterLogin(path);
           $location.path('/signin');
         }
       };
@@ -73,11 +74,26 @@ angular.module('2ViVe')
       GiftCard.prototype.clear = function() {
         ipCookie.remove('selectedGiftCard');
         ipCookie.remove('giftCardInfo');
+        ipCookie.remove('eventId');
       };
 
       GiftCard.prototype.populate = function() {
         this.info = ipCookie('giftCardInfo');
         this.selectedGiftCard = ipCookie('selectedGiftCard');
+        var eventId = ipCookie('eventId');
+        if (eventId) {
+          this.optionalFields = {
+            'event-code': eventId
+          };
+        }
+      };
+
+      GiftCard.prototype.bindEvent = function(eventId) {
+        ipCookie('eventId', eventId);
+      };
+
+      GiftCard.prototype.unBindEvent = function() {
+        ipCookie.remove('eventId');
       };
 
       GiftCard.prototype.placeOrder = function(creditcard) {
@@ -96,7 +112,8 @@ angular.module('2ViVe')
         return $http.post('/api/v2/giftcards', {
           'variant-id': this.selectedGiftCard.id,
           'creditcard': creditcard,
-          'email-info': this.info
+          'email-info': this.info,
+          'optional-fields': this.optionalFields
         }, {
           transformRequest: function(data) {
             return angular.toJson(dashlize(data));
